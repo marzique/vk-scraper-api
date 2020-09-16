bd7c895c6dd273dda69bc1b48ed4922807d621ce
marzique <marzique@gmail.com>
2020-09-17 01:37:49 +0300

    send album method

--- apps/poster/bot/client.py
+++ apps/poster/bot/client.py
@@ -8,22 +8,28 @@ from config.settings import TELEGRAM_TOKEN
 
 
 class TelegramClient:
-    CHAT_ID = '@ArsenalRepostsTest'  # channel name where messages will be sent
+    CHANNEL_ID = '@ArsenalRepostsTest'  # where messages will be sent
 
     def __init__(self):
         self.bot = telegram.Bot(token=TELEGRAM_TOKEN)
 
-    def send_message(self, text='', parse_mode='HTML'):
-        """TODO:
-        https://python-telegram-bot.readthedocs.io/en/stable/telegram.bot.html#telegram.Bot.send_message
-        """
+    def send_text(self, text, parse_mode='HTML'):
+        """Send text message. Text length should be 1-4096 symbols"""
 
-        try:
-            return self.bot.send_message(
-                chat_id=self.CHAT_ID, 
+        return self.bot.send_message(
+                chat_id=self.CHANNEL_ID, 
                 text=text, 
                 parse_mode=parse_mode
             )
-        except telegram.error.BadRequest:
-            return False
 
+    def send_album(self, photo_urls, caption=None):
+        """Send 2-10 photos with caption to channel"""
+
+        media_group = [telegram.InputMediaPhoto(url, parse_mode=None) for url in photo_urls]
+        media_group[0].caption = caption
+
+        message = self.bot.send_media_group(
+            chat_id=self.CHANNEL_ID, 
+            media=media_group
+            )
+        return message
